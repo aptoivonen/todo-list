c7b64588eafc986b8755e6c39e6973fc3ce09232
Aleksi Toivonen <aptoivonen@mail.com>
2021-04-21 10:37:09 +0300

    Reorganize pubsub topics

--- src/js/controller/controller.js
+++ src/js/controller/controller.js
@@ -1,26 +1,21 @@
 import todoDb from "../db/todoDb";
 import pubsub from "../pubsub/pubsub";
 
-function logTodo(todo) {
-  console.log("todo changed:", todo);
+function log(msg, data) {
+  console.log(msg, data);
 }
 
-function logTodoList(todoList) {
-  console.log("todoList changed:", todoList);
-}
-
-function initLog({ todoLists }) {
-  console.table("init log:", todoLists);
-}
-
-function createTodo({ title, description, dueDate, priority }) {
+function createTodo({ todoListId, title, description, dueDate, priority }) {
   const result = todoDb.createTodo({
+    todoListId,
     title,
     description,
     dueDate,
     priority,
   });
-  pubsub.publish("todo/change", result);
+  if (result) {
+    pubsub.publish("todo/created", result);
+  }
 }
 
 function saveTodo({ todoId, title, description, dueDate, priority }) {
@@ -31,38 +26,52 @@ function saveTodo({ todoId, title, description, dueDate, priority }) {
     dueDate,
     priority,
   });
-  pubsub.publish("todo/change", result);
+  if (result) {
+    pubsub.publish("todo/saved", result);
+  }
 }
 
 function removeTodo(todoId) {
   const result = todoDb.removeTodo(todoId);
-  pubsub.publish("todo/change", result);
+  if (result) {
+    pubsub.publish("todo/removed", { todoId, result });
+  }
 }
 
 function createTodoList(title) {
   const result = todoDb.createTodoList(title);
-  pubsub.publish("todoList/change", result);
+  if (result) {
+    pubsub.publish("todoList/created", result);
+  }
 }
 
 function saveTodoList({ todoListId, title }) {
   const result = todoDb.saveTodoList({ todoListId, title });
-  pubsub.publish("todoList/change", result);
+  if (result) {
+    pubsub.publish("todoList/saved", result);
+  }
 }
 
 function removeTodoList(todoListId) {
   const result = todoDb.removeTodoList(todoListId);
-  pubsub.publish("todoList/change", result);
+  if (result) {
+    pubsub.publish("todoList/removed", { todoListId, result });
+  }
 }
-
-pubsub.subscribe("init", initLog);
-pubsub.subscribe("todo/change", logTodo);
-pubsub.subscribe("todo/create", createTodo);
-pubsub.subscribe("todo/save", saveTodo);
-pubsub.subscribe("todo/remove", removeTodo);
-pubsub.subscribe("todoList/change", logTodoList);
-pubsub.subscribe("todoList/create", createTodoList);
-pubsub.subscribe("todoList/save", saveTodoList);
-pubsub.subscribe("todoList/remove", removeTodoList);
+pubsub
+  .subscribe("todo/created", log.bind(null, "todo/created"))
+  .subscribe("todo/saved", log.bind(null, "todo/saved"))
+  .subscribe("todo/removed", log.bind(null, "todo/removed"))
+  .subscribe("todoList/created", log.bind(null, "todoList/created"))
+  .subscribe("todoList/saved", log.bind(null, "todoList/saved"))
+  .subscribe("todoList/removed", log.bind(null, "todoList/removed"))
+  .subscribe("init", log.bind(null, "init"))
+  .subscribe("user/todo/create", createTodo)
+  .subscribe("user/todo/save", saveTodo)
+  .subscribe("user/todo/remove", removeTodo)
+  .subscribe("user/todoList/create", createTodoList)
+  .subscribe("user/todoList/save", saveTodoList)
+  .subscribe("user/todoList/remove", removeTodoList);
 
 todoDb.init();
 pubsub.publish("init", { todoLists: todoDb.getTodoLists() });
