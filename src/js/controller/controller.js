37cf378c7236c2d995ad8c40898f304011b1c300
Aleksi Toivonen <aptoivonen@mail.com>
2021-04-26 16:18:47 +0300

    Refactor and add handle active todolist

--- src/js/controller/controller.js
+++ src/js/controller/controller.js
@@ -2,19 +2,44 @@ class Controller {
   constructor(model, view) {
     this.model = model;
     this.view = view;
+    this.activeTodoListId;
 
     this.onTodoListsChanged(this.model.getTodoLists());
+
+    this._bind();
+  }
+
+  _bind() {
     this.view.todoListView.bindAddTodoListHandler(
       this.handleAddTodoList.bind(this)
     );
+    this.view.todoListView.bindEditTodoListHandler(
+      this.handleEditTodoList.bind(this)
+    );
     this.view.todoListView.bindDeleteTodoListHandler(
       this.handleDeleteTodoList.bind(this)
+    );
+    this.view.todoListView.bindActivateTodoListHandler(
+      this.handleActiveTodoList.bind(this)
     );
     this.model.bindTodoListsChanged(this.onTodoListsChanged.bind(this));
   }
 
   onTodoListsChanged(todoLists) {
-    this.view.todoListView.setTodoLists(todoLists);
+    this.view.todoListView.updateTodoLists(todoLists);
+    if (todoLists.length === 0) {
+      this.activeTodoListId = undefined;
+      return;
+    }
+    const foundTodoList = todoLists.find(
+      (todoList) => todoList.id === this.activeTodoListId
+    );
+    console.log("foundTodoList:", foundTodoList);
+    console.log("activeTodoListId:", this.activeTodoListId);
+    if (!this.activeTodoListId || !foundTodoList) {
+      console.log("reset lists to id 0");
+      this.handleActiveTodoList(todoLists[0].id);
+    }
   }
 
   handleAddTodoList(title) {
@@ -27,6 +52,11 @@ class Controller {
 
   handleDeleteTodoList(id) {
     this.model.deleteTodoList(id);
+  }
+
+  handleActiveTodoList(id) {
+    this.activeTodoListId = id;
+    this.view.todoListView.setActiveTodoList(id);
   }
 }
 
