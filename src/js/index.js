d666f4a24b0f3dbd1893452dd114fdc66374030a
Aleksi Toivonen <aptoivonen@mail.com>
2021-05-05 15:15:04 +0300

    Clean index.js

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,4 @@ import Model from "./models/model";
 import View from "./views/view";
 import Controller from "./controller/controller";
 
-const model = new Model();
-const app = new Controller(model, new View());
-window.model = model;
+const app = new Controller(new Model(), new View());
