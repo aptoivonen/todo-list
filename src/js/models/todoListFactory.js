898243a58bf932057f0d9b258a4b1a216974d155
Aleksi Toivonen <aptoivonen@mail.com>
2021-05-06 11:14:29 +0300

    Change default todo list name

--- src/js/models/todoListFactory.js
+++ src/js/models/todoListFactory.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from "uuid";
 function createTodoList({ type, title = "" }) {
   switch (type) {
     case "default":
-      return new TodoList({ id: "0", title: "default", removable: false });
+      return new TodoList({ id: "0", title: "Home", removable: false });
     case "standard":
       return new TodoList({ id: uuidv4(), title, removable: true });
   }
